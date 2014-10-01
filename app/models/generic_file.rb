400da3b332de89409d5704b2d83042e6ebb3967f
Joel Ferrier <jferrier45@gmail.com>
2014-09-30 17:21:18 -0700

    implement get_exif_metadata

--- app/models/generic_file.rb
+++ app/models/generic_file.rb
@@ -3,4 +3,26 @@ class GenericFile < ActiveFedora::Base
 
   has_metadata "osfMetadata", type: Datastreams::PlayMetadata
   has_attributes :play, :playwright, :year, datastream: :osfMetadata, multiple: false
+
+  def get_exif_metadata(generic_file)
+  	# requires Perl-Image-Exif-Tool -- needs to be added to vagrant box
+  	# requires exiftool gem
+  	# TODO: error checking
+
+  	# fetch bytestring of content
+  	content_stream = generic_file.datastreams.fetch('content').content
+
+  	tmp_filename = Sufia.config.temp_file_base+'/tmp_'+generic_file.title.to_s
+  	tmp_file = File.new(tmp_filename, 'w')
+  	tmp_file.binmode
+  	tmp_file.write content_stream
+
+  	exif_hash = Exiftool.new(tmp_filename).to_hash
+
+  	File.delete(tmp_file)
+
+  	return exif_hash
+
+  end
+
 end
