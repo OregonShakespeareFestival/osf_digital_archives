74cbfa02c716dd2c72b73e6c830e1b0d827b0b7b
Jeff Parr & Joel Ferrier <team+jeff+joel@codingzeal.com>
2014-10-13 10:48:27 -0700

    fix production_credits schema to use namespaced tables

--- vendor/engines/production_credits/db/migrate/20141003001625_add_production_schema.rb
+++ vendor/engines/production_credits/db/migrate/20141003001625_add_production_schema.rb
@@ -1,18 +1,18 @@
 class AddProductionSchema < ActiveRecord::Migration
   def change
-    create_table :works do |t|
+    create_table :production_credits_works do |t|
       t.string :title
       t.string :medium # Play, Dance, ...
       t.integer :year
     end
 
-    create_table :productions do |t|
+    create_table :production_credits_productions do |t|
       t.belongs_to :work, index: true
       t.date :open_on
       t.date :close_on
     end
 
-    create_table :credits do |t|
+    create_table :production_credits_credits do |t|
       t.string :credit_type # nil, original, understudy, swing, cover
       t.belongs_to :work, index: true
       t.belongs_to :production, index: true
@@ -21,30 +21,30 @@ class AddProductionSchema < ActiveRecord::Migration
       t.belongs_to :person, index: true
     end
 
-    create_table :roles do |t|
+    create_table :production_credits_roles do |t|
       t.string :category # cast, crew, production, direction, writing, department, thanks
       t.string :name
       t.belongs_to :work, index: true
       t.belongs_to :production, index: true
     end
 
-    create_table :people do |t|
+    create_table :production_credits_people do |t|
       t.string :denormalized_full_name
       t.string :disambiguation # For reference, IMDB uses "(I)", "(II)", etc.
       t.date :date_of_birth
     end
 
-    create_table :performances do |t|
+    create_table :production_credits_performances do |t|
       t.belongs_to :production, index: true
       t.datetime :performed_at
     end
 
-    create_table :venues do |t|
+    create_table :production_credits_venues do |t|
       t.string :denormalized_name
       t.date :opened_on
     end
 
-    create_table :names do |t|
+    create_table :production_credits_names do |t|
       t.string :full_name
       t.belongs_to :person, index: true
       t.belongs_to :venue, index: true
