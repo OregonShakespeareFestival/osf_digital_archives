debc3f443938d0fdd4b18868b6448dd835a5f6a8
Matti Salokangas <matti@codingzeal.com>
2014-10-14 14:57:02 -0700

    Add credit type enum to credit

--- vendor/engines/production_credits/app/models/production_credits/credit.rb
+++ vendor/engines/production_credits/app/models/production_credits/credit.rb
@@ -4,6 +4,9 @@ module ProductionCredits
     belongs_to :role
     belongs_to :person
 
+    def credit_type_enum
+      ['Cast', 'Crew', 'Production', 'Direction', 'Writing', 'Department', 'Thanks']
+    end
 
     def name
       "#{person.denormalized_full_name} - #{role.name}"
