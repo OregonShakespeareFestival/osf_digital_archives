0128b5f3a046c65c3986f3b701e628fb1f364ce7
Matti Salokangas <matti@codingzeal.com>
2014-10-14 15:03:44 -0700

    Add category enum to Roles to provide static list in UI

--- vendor/engines/production_credits/app/models/production_credits/role.rb
+++ vendor/engines/production_credits/app/models/production_credits/role.rb
@@ -3,5 +3,9 @@ module ProductionCredits
     belongs_to :work
     belongs_to :production
     has_many :credits
+
+    def category_enum
+      ['Cast', 'Crew', 'Production', 'Direction', 'Writing', 'Department', 'Thanks']
+    end
   end
 end
