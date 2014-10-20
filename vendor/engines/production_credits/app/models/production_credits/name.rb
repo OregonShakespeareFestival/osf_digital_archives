3cb6c5f3c65cde6e5fbec2aae7638ea07fa43961
Jeff Parr & Joel Ferrier <team+jeff+joel@codingzeal.com>
2014-10-20 11:17:20 -0700

    validate presence of person or venue

--- vendor/engines/production_credits/app/models/production_credits/name.rb
+++ vendor/engines/production_credits/app/models/production_credits/name.rb
@@ -5,9 +5,17 @@ module ProductionCredits
 
     validates_presence_of :full_name
     validates_presence_of :cannonized_at
+    validates_presence_of :person_or_venue
 
     def name
       full_name
     end
+
+    def person_or_venue
+      if [self.person, self.venue].reject(&:blank?).size != 1
+        errors[:base] << ("A Name must be associated with either a Person or a Venue")
+      end
+    end
+    
   end
 end
