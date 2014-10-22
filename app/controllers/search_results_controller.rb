da4c6e457a3b6f234c002eb7c0b8cef5fbf2dfa3
Matti Salokangas <matti@codingzeal.com>
2014-10-22 15:57:57 -0700

    Use super class to retrieve search results

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -1,8 +1,22 @@
 class SearchResultsController < CatalogController
   layout "osf-client/application"
 
+  def index
+    # Returns a Blacklight::SolrResponse
+    #  - See http://www.rubydoc.info/github/projectblacklight/blacklight/Blacklight/SolrResponse
+    (@response, @document_list) = get_search_results
+
+    filter(@response)
+  end
+
+  private
+
+  # TODO: Filter @response.docs by filter criteria.  Venue, date, document type, etc
+  def filter(response)
+
+  end
+
 end
-# look in blacklight/lib/blacklight catalog
 
 
 
