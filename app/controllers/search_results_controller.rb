c5b0126b6db8c4f20c3f8657ebfc5c7ab1f6be02
Matti Salokangas <matti@codingzeal.com>
2014-10-22 17:09:05 -0700

    Query solr by resource type and stub filter method

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -4,16 +4,22 @@ class SearchResultsController < CatalogController
   def index
     # Returns a Blacklight::SolrResponse
     #  - See http://www.rubydoc.info/github/projectblacklight/blacklight/Blacklight/SolrResponse
-    (@response, @document_list) = get_search_results
+    (videoResponse, video_document_list) = get_search_results({q: params["q"], f: { desc_metadata__resource_type_sim: ["Video"]} })
+    (imageResponse, image_document_list) = get_search_results({q: params["q"], f: { desc_metadata__resource_type_sim: ["Image"]} })
+    (articleResponse, article_document_list) = get_search_results({q: params["q"], f: { desc_metadata__resource_type_sim: ["Article"]} })
+    (audioResponse, audio_document_list) = get_search_results({q: params["q"], f: { desc_metadata__resource_type_sim: ["Audio"]} })
 
-    filter(@response)
+    @videos = filter(videoResponse.docs)
+    @images = filter(imageResponse.docs)
+    @articles = filter(articleResponse.docs)
+    @audio = filter(audioResponse.docs)
   end
 
   private
 
   # TODO: Filter @response.docs by filter criteria.  Venue, date, document type, etc
   def filter(response)
-
+    response
   end
 
 end
