4018dada71a59ed17af7801a76fbd8113479eae1
Matti Salokangas <matti@codingzeal.com>
2014-10-30 10:34:45 -0700

    Add paging parameters

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -14,7 +14,7 @@ class SearchResultsController < CatalogController
     resource_types = params[:t] ? params[:t].split(',') : ['images', 'videos', 'audios', 'articles']
 
     response = resource_types.each_with_object({}) do |type, obj|
-      obj[type] = do_search(type.singularize.capitalize, params[type + '_page'])
+      obj[type] = do_search(type.singularize.capitalize, params[type + '_page'], params[type + '_per_page'])
     end
 
     render json: response.merge({ 'filters' => @filters, 'query' => @query })
@@ -22,9 +22,9 @@ class SearchResultsController < CatalogController
 
   private
 
-  def do_search(resource_type, page)
+  def do_search(resource_type, page, per_page)
     # TODO: Pass page to Solr to retrieve correct page
-    (res, res_document_list) = get_search_results({q: @query, f: { desc_metadata__resource_type_sim: [resource_type]} })
+    (res, res_document_list) = get_search_results({q: @query, f: { desc_metadata__resource_type_sim: [resource_type]}, page: page, per_page: per_page })
 
     res = filter(res)
 
@@ -34,13 +34,17 @@ class SearchResultsController < CatalogController
         'id' => d[:id][6..-1],
         'access' => d[:read_access_group_ssim].first,
         'type' => d[:desc_metadata__resource_type_tesim].first,
-        'title' => d[:desc_metadata__title_tesim].first
+        'title' => d[:desc_metadata__title_tesim].first,
+        'thumbnail' => nil,
+        'keywords' => nil,
+        'date' => nil,
+        'production' => nil
       }
     }
 
     return {
-      'current_page' => 0,
-      'items_per_page' => 10,
+      'current_page' => page,
+      'items_per_page' => per_page,
       'total_items' => res.response['numFound'],
       'data' => data
     }
