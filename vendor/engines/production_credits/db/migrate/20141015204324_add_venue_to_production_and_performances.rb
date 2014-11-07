939b988e187b1747fd6a20778d7b0abdf682f9cc
Jeff Parr & Joel Ferrier <team+jeff+joel@codingzeal.com>
2014-11-06 21:11:49 -0800

    drop preexisting productions tables

--- vendor/engines/production_credits/db/migrate/20141015204324_add_venue_to_production_and_performances.rb
+++
@@ -1,6 +0,0 @@
-class AddVenueToProductionAndPerformances < ActiveRecord::Migration
-  def change
-    add_column :production_credits_productions, :venue_id, :integer
-    add_column :production_credits_performances, :venue_id, :integer
-  end
-end
--- vendor/engines/production_credits/db/migrate/20141020172105_add_alias_to_credit.rb
+++
@@ -1,6 +0,0 @@
-class AddAliasToCredit < ActiveRecord::Migration
-  def change
-    add_column :production_credits_credits, :name_id, :integer
-  end
-end
-  
