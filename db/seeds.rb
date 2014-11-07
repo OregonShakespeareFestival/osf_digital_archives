1ae7d14507e0bba1c442bdbd69a0334dcdc750e0
Trever Yarrish <trever@codingzeal.com>
2014-11-06 16:44:07 -0800

    add myself to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ User.find_or_create_by!(email: "mariad@osfashland.org") { |user| user.password =
 User.find_or_create_by!(email: "debrag@osfashland.org") { |user| user.password = "osfarchive2014"; user.display_name = "Debra Griffith" }
 
 User.find_or_create_by!(email: "robert.head@codingzeal.com") { |user| user.password = "osfarchive2014"; user.display_name = "Robert Head" }
+User.find_or_create_by!(email: "trever@codingzeal.com") { |user| user.password = "osfarchive2014"; user.display_name = "Trever Yarrish" }
 User.find_or_create_by!(email: "jeff.parr@codingzeal.com") { |user| user.password = "osfarchive2014"; user.display_name = "Jeff Parr" }
 User.find_or_create_by!(email: "sean.culver@codingzeal.com") { |user| user.password = "osfarchive2014"; user.display_name = "Sean Culver" }
 
