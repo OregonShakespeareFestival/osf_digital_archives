d853a2e80e0a65c131e31690ad08a76ac0f44cf6
Jeff Parr & Joel Ferrier <team+jeff+joel@codingzeal.com>
2014-11-06 21:23:50 -0800

    rm old models

--- vendor/engines/production_credits/spec/factories/production_credits_works.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :production_credits_work, :class => 'Work' do
-    title "MyString"
-    medium "MyString"
-    year 1
-  end
-end
--- vendor/engines/production_credits/spec/models/production_credits/work_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-module ProductionCredits
-  describe Work, :type => :model do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-end
