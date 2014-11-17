60322ccc412af1c2b439c9f3a58a5b0f9a6ac4c6
Jeff Parr and Robert Head <team+jeff+rob@codingzeal.com>
2014-11-17 11:10:17 -0800

    whitespace

--- app/models/datastreams/production_data.rb
+++ app/models/datastreams/production_data.rb
@@ -1,17 +1,14 @@
 class Datastreams::ProductionData < ActiveFedora::OmDatastream
+  set_terminology do |t|
+    t.root(path: "fields")
+    t.production_name index_as: [:stored_searchable, :facetable]
+  end
 
-	set_terminology do |t|
-		t.root(path: "fields")
-		t.production_name index_as: [:stored_searchable, :facetable]
-		
-	end
+  def self.xml_template
+    Nokogiri::XML.parse("<fields/>")
+  end
 
-	def self.xml_template
-		Nokogiri::XML.parse("<fields/>")
-	end
-
-	def prefix
-		''
-	end
-
+  def prefix
+    ''
+  end
 end
