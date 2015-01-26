a8ea90e3cf0def57912de6bd90f2c12331c22486
Jeff Parr <jeffparr100@gmail.com>
2015-01-26 12:04:15 -0800

    remove medium from work [#86136442]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110233153) do
+ActiveRecord::Schema.define(version: 20150126193826) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "user_id",       null: false
@@ -166,7 +166,6 @@ ActiveRecord::Schema.define(version: 20141110233153) do
   create_table "production_credits_works", force: true do |t|
     t.string   "title"
     t.string   "author"
-    t.string   "medium"
     t.date     "year_written"
     t.text     "description"
     t.datetime "created_at"
